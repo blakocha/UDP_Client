1d910f7855bba6d5296288af67c92d8508d883e5
student <student@PC-217>
2017-05-25 18:17:32 +0200

    UDP Client 1

--- Form1.cs
+++ Form1.cs
@@ -20,8 +20,23 @@ namespace WindowsFormsApplication3
 
         private void button_wyslij_Click(object sender, EventArgs e)
         {
-            string host = Adres.Text; 
-            int port = Convert.ToUInt16(PortNumber)
+            string host = Adres.Text;
+            int port = Convert.ToUInt16(PortNumber.Value);
+
+            try
+            {
+                UdpClient klient = new UdpClient(host, port);
+                Byte[] dane = Encoding.ASCII.GetBytes(Wiadomosci.Text);
+                klient.Send(dane, dane.Length);
+                info_o_polaczeniu.Items.Add("Wysyłanie wiadomości do hosta " + host + ": " + port);
+                klient.Close();
+            } 
+
+            catch (Exception ex)
+            {
+                info_o_polaczeniu.Items.Add("Błąd. Nie udało się wysłać wiadomości! ");
+                MessageBox.Show(ex.ToString(), "Błąd");
+            }
         }
     }
 }
